cf5de357da2e5e9c44d87f064946a15a61aa4f4a
SauravBelwal <96074581+SauravBelwal@users.noreply.github.com>
2023-02-05 09:35:48 +0530

    Last changes

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,7 +1,5 @@
 /*================MENU SHOW============ */
-//   import Swiper from 'swiper/swiper-bundle.esm.js';
-//    import Swiper from 'swiper/swiper-bundle.esm.js';
-//     import 'swiper/swiper-bundle.css';
+
 
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle');
@@ -114,7 +112,7 @@ const navMenu = document.getElementById('nav-menu'),
 
 
         /*============================ PORTFOLIO SWIPER ============================== */
-          let swiper = new Swiper('.portfolio_container', {
+          var swiper = new Swiper(".swiper", {
               cssMode: true,
               loop: true,
               navigation: {
@@ -126,6 +124,60 @@ const navMenu = document.getElementById('nav-menu'),
               clickable: true,
             },
         });
+
+        /*===================== TESTIMONIAL ================== */
+             let swiperTestimonial = new Swiper('.testimonial_container', {
+              grabCursor: true,
+              loop: true,
+              spaceBetween: 48,
+              pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+              dynamicBullets: true,
+            },
+            breakpoints:{
+                568:{
+                    slidesPerView: 2,
+                }
+            }
+        });
+
+        /*================================== SCROLL SECTION ACTIVE LINK ====================== */
+        const sections = document.querySelectorAll('section[id]')
+
+        function scrollActive(){
+         const scrollY = window.pageYOffset
+
+        sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+           })
+        }
+         window.addEventListener('scroll', scrollActive)
+
+         /*==================== CHANGE BACKGROUND HEADER ======================= */
+         function scrollHeader(){
+         const nav = document.getElementById('header')
+         // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+          if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+        }
+         window.addEventListener('scroll', scrollHeader)
+
+         /*========================== SHOW SCROLL UP ====================== */
+        function scrollTop(){
+        const scrollTop = document.getElementById('scroll-top');
+         // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+         if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+        }
+        window.addEventListener('scroll', scrollTop)
+
 
 
 
